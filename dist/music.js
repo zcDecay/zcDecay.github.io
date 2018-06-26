f1cc7e975347fba6f4f1814eec32921c819b6ead
SummerAddiction <1105544546@qq.com>
2018-06-26 11:17:33 +0800

    Site updated: 2018-06-26 11:17:31

--- dist/music.js
+++ dist/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    theme: '#e9e9e9',
     audio: [
       {
         name: "love yourself",
